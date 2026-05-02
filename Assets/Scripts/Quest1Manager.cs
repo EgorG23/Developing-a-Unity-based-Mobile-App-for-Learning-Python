8ff28e71a663b179e2b386781381ef57663cb2cf
EgorG23 <gusarow.egor2017@yandex.ru>
2026-05-02 20:05:42 +0300

    Add QuestManager for the practice of the lesson1 (2nd lesson)

--- Assets/Scripts/Quest1Manager.cs
+++ Assets/Scripts/Quest1Manager.cs
@@ -2,15 +2,69 @@ using UnityEngine;
 
 public class Quest1Manager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static Quest1Manager Instance;
+
+    void Awake()
     {
-        
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public bool powerFixed = false;
+    public bool pcUnlocked = false;
+    public bool codeCorrect = false;
+
+    [Header("Screens")]
+    public GameObject pcScreenPrefab;
+    public GameObject doorOpenPrefab;
+    public GameObject winScreenPrefab;
+
+    public void FixPower()
     {
-        
+        powerFixed = true;
+        Debug.Log("Питание включено");
+
+        PracticeManager.Instance.ShowScreen(pcScreenPrefab);
+    }
+
+    public void UnlockPC()
+    {
+        if (!powerFixed)
+        {
+            Debug.Log("Нет тока");
+            return;
+        }
+
+        pcUnlocked = true;
+        Debug.Log("ПК включен");
+    }
+
+    public void CheckCode(string code)
+    {
+        bool nameOK = code.Contains("user_name = \"Alex\"");
+        bool passOK = code.Contains("user_password = 1234567890");
+        bool print1 = code.Contains("print(user_name)");
+        bool print2 = code.Contains("print(user_password)");
+
+        if (nameOK && passOK && print1 && print2)
+        {
+            codeCorrect = true;
+            Debug.Log("Код верный");
+
+            OpenDoor();
+        }
+        else
+        {
+            Debug.Log("Код неверный");
+        }
+    }
+
+    void OpenDoor()
+    {
+        PracticeManager.Instance.ShowScreen(doorOpenPrefab);
+    }
+
+    public void FinishQuest()
+    {
+        PracticeManager.Instance.ShowScreen(winScreenPrefab);
     }
 }
